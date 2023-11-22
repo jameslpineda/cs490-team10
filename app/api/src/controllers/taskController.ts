636f32215a9aab545b88bf32978da4b1141f777a
rubykap30 <78500275+rubykap30@users.noreply.github.com>
2023-11-21 23:16:59 -0500

    Fixed taskUpdate endpoint and put services in proper location

--- app/api/src/controllers/taskController.ts
+++ app/api/src/controllers/taskController.ts
@@ -96,8 +96,6 @@ export const updateTaskHandler = asyncHandler(
     if (req.body.date) {
       data.date = moment(req.body.date).startOf('day').toDate();
     }
-    console.log('DATA:' + data);
-    console.log('TASK ID' + task_id);
     const updatedTask = await updateTask({ _id: task_id }, data);
 
     if (!updatedTask) {
