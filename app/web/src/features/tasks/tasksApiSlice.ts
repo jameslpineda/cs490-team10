cf6aa148e7aa92308c8a5d1314984c8e9b2071aa
James Pineda <jlp4@njit.edu>
2023-11-27 23:20:26 -0500

    FEAT-TaskModelUpdate: Implemented taskApiSlice

---
+++ app/web/src/features/tasks/tasksApiSlice.ts
@@ -0,0 +1,85 @@
+import { apiSlice } from '../../app/api/apiSlice';
+import { toast } from 'react-toastify';
+
+type CreateTaskPayload = {
+  name: string;
+  notes: string;
+  status: string;
+  priority: string;
+  timers: number;
+};
+type CreateTaskResponse = {
+  message: string;
+};
+
+type UpdateTaskPayload = {
+  name: string;
+  status: string;
+  timers: number;
+  completed_timers: number;
+  notes: string;
+  priority: string;
+  date: Date | string;
+};
+type UpdateTaskResponse = {
+  message: string;
+  updateFields: string[];
+};
+
+export const tasksApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getTasks: builder.query({
+      query: (date) => ({
+        url: `/task/retrieve?date=${date}`,
+        method: 'GET',
+      }),
+      providesTags: ['Tasks'],
+    }),
+    createUser: builder.mutation<CreateTaskResponse, CreateTaskPayload>({
+      query: (payload) => ({
+        url: '/task/create',
+        method: 'POST',
+        body: payload,
+      }),
+      invalidatesTags: ['Tasks'],
+    }),
+    updateTask: builder.mutation<UpdateTaskResponse, UpdateTaskPayload>({
+      query: (payload) => ({
+        url: '/user/update',
+        method: 'POST',
+        body: payload,
+      }),
+      invalidatesTags: ['Tasks'],
+      async onQueryStarted(arg, { queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
+          console.log('DATA: ', data);
+
+          toast.success(`${data.message}!`, {
+            position: toast.POSITION.TOP_CENTER,
+            autoClose: 5000,
+          });
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (err: any) {
+          if (err?.error?.data?.message) {
+            toast.error(err?.error?.data?.message, {
+              position: toast.POSITION.TOP_CENTER,
+              autoClose: 7000,
+            });
+          } else {
+            toast.error('An unexpected error has occurred', {
+              position: toast.POSITION.TOP_CENTER,
+              autoClose: 7000,
+            });
+          }
+        }
+      },
+    }),
+  }),
+});
+
+export const {
+  useGetTasksQuery,
+  useCreateUserMutation,
+  useUpdateTaskMutation,
+} = tasksApiSlice;
