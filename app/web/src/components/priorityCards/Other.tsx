a8693810d31662dedbde77484cf6c05c85c33c7b
Anbar Saleem <45743448+anbarsaleem@users.noreply.github.com>
2023-12-03 01:42:47 -0500

    completed drag and drop task implementation end2end with prio update

--- app/web/src/components/priorityCards/Other.tsx
+++ app/web/src/components/priorityCards/Other.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import Task from '../Task';
+import { useDroppable } from '@dnd-kit/core';
 import { TaskCardProps, TaskProps } from '../../interfaces/taskInterface';
 
 const Other: React.FC<TaskCardProps> = ({ tasks }) => {
+  const { setNodeRef, isOver } = useDroppable({
+    id: 'Other',
+  });
   return (
     <>
-      <div className="flex-1 bg-gray-100 rounded-md p-4 mb-4">
-        <h2 className="text-xl font-semibold font-sans">Other</h2>
-        {tasks.map((task: TaskProps) => (
-          <Task
-            key={task._id}
-            {...task}
-          />
-        ))}
+      <div ref={setNodeRef}>
+        <div
+          className="flex-1 bg-gray-100 rounded-md p-4 mb-4"
+          style={{ border: isOver ? '2px solid grey' : '' }}
+        >
+          <h2 className="text-xl font-semibold font-sans">Other</h2>
+          {tasks.map((task: TaskProps) => (
+            <Task
+              key={task._id}
+              {...task}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
