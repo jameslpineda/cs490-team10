4469eaadacaa4c359541b43f59256322971580bf
Abdul Lateef <abdullateefv@gmail.com>
2023-12-05 17:38:18 -0500

    Update DayCalendar.tsx

--- app/web/src/components/calendarComponents/DayCalendar.tsx
+++ app/web/src/components/calendarComponents/DayCalendar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import React, { useCallback } from 'react';
 import moment from 'moment';
 import Calendar from './Calendar';
 import { Event } from '../../interfaces/eventInterface';
@@ -9,16 +10,31 @@ interface DayCalendarProps {
 }
 
 const DayCalendar: React.FC<DayCalendarProps> = ({ date, events }) => {
+  const slotPropGetter = useCallback(
+    (date: moment.MomentInput) => ({
+      className: 'rbc-label',
+      ...(moment(date).hour() == moment().hour() && {
+        style: {
+          color: '#6284ff',
+        },
+      }),
+    }),
+    [],
+  );
+
   return (
     <Calendar
       events={events}
       toolbar={false}
       defaultView="day"
+      slotPropGetter={slotPropGetter}
       min={moment(date + 'T05:00:00').toDate()}
       max={moment(date + 'T20:59:00').toDate()}
+      max={moment(date + 'T23:59:00').toDate()}
       date={moment(date).toDate()}
     />
   );
 };
 
 export default DayCalendar;
+
