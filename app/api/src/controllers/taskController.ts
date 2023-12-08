a56da5b54b2a239b326cd7b4f2ffb008b3e5bffa
rubykap30 <78500275+rubykap30@users.noreply.github.com>
2023-12-08 15:52:39 -0500

    CT-25 Task Status Updates and Creates New Task

--- app/api/src/controllers/taskController.ts
+++ app/api/src/controllers/taskController.ts
@@ -112,3 +112,115 @@ export const updateTaskHandler = asyncHandler(
     });
   },
 );
+
+// @desc Creates new planned task list for next day
+// @route POST /task/plan
+// @access Private
+export const planTaskHandler = async (
+  req: AuthRequest,
+  res: Response,
+): Promise<void> => {
+  try {
+    const date = req.query.date;
+
+    if (!date) {
+      res.status(422).json({ error: 'Missing date in query' });
+      return;
+    }
+
+    const from = moment(date as string)
+      .subtract(1, 'days')
+      .startOf('day');
+    const to = moment(from).add(1, 'day');
+
+    const today = moment(date as string)
+      .startOf('day')
+      .toDate();
+
+    // Get all tasks by date
+    const data = {
+      user_id: req.user_id,
+      date: { $gte: from.toDate(), $lt: to.toDate() },
+    };
+    const tasksToUpdate = await getTasksByDate(data);
+
+    // Adjust here for empty task list retrieved
+    if (!tasksToUpdate || tasksToUpdate.length === 0) {
+      console.log('No tasks for planning');
+      res.status(200).json({
+        message: 'No tasks for planning',
+        updatedTasks: 0,
+        createdTasks: 0,
+      });
+      return;
+    }
+
+    const updatedTasks = await Promise.all(
+      tasksToUpdate.map(async (task) => {
+        try {
+          // Make a service to filter out completed and cancelled tasks
+          if (
+            task.status !== 'Task is complete' &&
+            task.status !== 'Task is deleted'
+          ) {
+            await updateTask(
+              { _id: task._id, status: 'Task has not been started' },
+              { status: 'Task rolled over to the next day' },
+            );
+          }
+          return true;
+        } catch (error) {
+          console.error('Error updating task:', error);
+          return false;
+        }
+      }),
+    );
+
+    const newTasks = tasksToUpdate.map((task) => ({
+      user_id: task.user_id,
+      notes: task.notes,
+      name: task.name,
+      timers: task.timers,
+      completed_timers: task.completed_timers,
+      // Change priority here
+      priority: task.priority,
+      status: 'Task has not been started',
+      date: today,
+    }));
+
+    console.log(newTasks);
+
+    const createdTasks = [];
+
+    for (const newTaskData of newTasks) {
+      try {
+        console.log(newTaskData);
+        const createdTask = await createTask(newTaskData);
+        if (createdTask) {
+          createdTasks.push(createdTask);
+          console.log('Task created successfully:', createdTask);
+        } else {
+          console.error('Failed to create task. createTask returned null.');
+        }
+      } catch (error) {
+        console.error('Error creating task:', error);
+      }
+    }
+
+    if (
+      !updatedTasks.every(Boolean) ||
+      createdTasks.length !== newTasks.length
+    ) {
+      throw new Error('Failed to update tasks');
+    }
+
+    res.status(200).json({
+      message: 'Tasks updated successfully',
+      updatedTasks: updatedTasks.length,
+      createdTasks: createdTasks.length,
+    });
+  } catch (error) {
+    console.error('Error in planTaskHandler:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
