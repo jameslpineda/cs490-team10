62cb76a2c15c2d0bb14dbdb189fe25fffc5d2f4f
rubykap30 <78500275+rubykap30@users.noreply.github.com>
2023-12-12 16:45:09 -0500

    CT-25-Tasks Plan Day Function Implements Rollover + Priority

--- app/api/src/controllers/taskController.ts
+++ app/api/src/controllers/taskController.ts
@@ -208,6 +208,62 @@ export const planTaskHandler = async (
       }
     }
 
+    // Retrives all tasks for current day
+    const todayData = {
+      user_id: req.user_id,
+      date: { $gte: today, $lt: moment(today).add(1, 'day').toDate() },
+    };
+    const todayTasks = await getTasksByDate(todayData);
+
+    // Updated priorities logic
+    if (todayTasks != null) {
+      const sortedTasks = todayTasks.sort((a, b) => {
+        // Sort tasks by priority (custom order: Top Priority > Important > Other)
+        const priorityOrder: { [key: string]: number } = {
+          'Top Priority': 0,
+          Important: 1,
+          Other: 2,
+        };
+
+        return (
+          priorityOrder[a.priority as string] -
+          priorityOrder[b.priority as string]
+        );
+      });
+      // Select the top three tasks as "Top Priority"
+      const topPriorityTasks = sortedTasks.slice(0, 3);
+
+      // Select remaining important tasks
+      const importantTasks = sortedTasks
+        .slice(3)
+        .filter((task) => task.priority === 'Important');
+
+      // If there are no important tasks, move all remaining other items to important
+      if (importantTasks.length === 0) {
+        const remainingTasks = sortedTasks.slice(3);
+        for (const task of remainingTasks) {
+          await updateTask({ _id: task._id }, { priority: 'Important' });
+        }
+      } else {
+        // If there were important items, leave other items as other
+        const remainingOtherTasks = sortedTasks
+          .slice(3)
+          .filter((task) => task.priority === 'Other');
+        for (const task of remainingOtherTasks) {
+          await updateTask({ _id: task._id }, { priority: 'Other' });
+        }
+      }
+
+      // Update priority for tasks in Top Priority
+      for (const task of topPriorityTasks) {
+        await updateTask({ _id: task._id }, { priority: 'Top Priority' });
+      }
+
+      // Update priority for tasks in remaining important tasks
+      for (const task of importantTasks) {
+        await updateTask({ _id: task._id }, { priority: 'Important' });
+      }
+    }
     // Error handling
     if (
       !updatedTasks.every(Boolean) ||
